56941e7e04bddceaae3c7f143c2223124bf36144
Andrew Fox <73478849+afox797@users.noreply.github.com>
2022-01-10 21:31:40 -0500

    testing things

---
+++ College_Student.java
@@ -0,0 +1,9 @@
+public class College_Student {
+
+    private College_Student() {
+        String name = "Andrew Fox";
+        String major = "Computer Science";
+        String society = null;
+        int year = 3;
+    }
+}
